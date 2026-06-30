7ad9b6f7e3ae46149dc099961db34335737c76e6
Muhammad Hamza Khan <Khumza050@gmail.com>
2026-06-30 15:38:01 +0500

    Add head deletion functionality in linked list
    Implemented a function to delete the head node of a linked list and updated the main function to demonstrate its usage.

---
+++ Linked List/deletion_of_head_in_LL.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+#include <vector>
+class Node{
+    public:
+    int data;
+    Node*ptr;
+    Node(int data1){
+        ptr = nullptr;
+        data=data1;
+    }
+    Node(int data1 , Node*Ptr){
+        data = data1;
+        ptr = Ptr;
+    }
+};
+Node*convertarr2ll(vector<int>&arr){
+    Node*head = new Node (arr[0]);
+    Node*movers = head;
+    for (int i = 1 ; i < arr.size() ; i++){
+        Node*temp = new Node (arr[i]);
+        movers->ptr = temp; 
+        movers = temp;
+    }
+    return head;
+}
+void traversalLL(Node*head){
+    Node*temp = head;
+    while(temp!=nullptr){
+        cout << temp->data<<" ";
+        temp = temp->ptr;
+    }
+}
+Node*deletehead(Node*head){
+    if (head==nullptr){
+        return nullptr;
+    }
+    Node*temp = head;
+    head = head->ptr;
+    delete temp;
+    return head;
+}
+int main(){
+    vector<int>ARR = {33,44,55,66,77};
+    Node*node = convertarr2ll(ARR);
+    node = deletehead(node);
+    traversalLL(node);
+}
